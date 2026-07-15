76b383cef22c3f380883f95614e3fa770590b30b
nullptr03 <nullptr03@singkolab.my.id>
2026-07-15 14:39:54 +0800

    drivers: video: backlight: Notify for brightness change
    From taoyao-s-oss

--- drivers/video/backlight/backlight.c
+++ drivers/video/backlight/backlight.c
@@ -126,6 +126,7 @@ static void backlight_generate_event(struct backlight_device *bd,
 	envp[1] = NULL;
 	kobject_uevent_env(&bd->dev.kobj, KOBJ_CHANGE, envp);
 	sysfs_notify(&bd->dev.kobj, NULL, "actual_brightness");
+	sysfs_notify(&bd->dev.kobj, NULL, "brightness");
 }
 
 static ssize_t bl_power_show(struct device *dev, struct device_attribute *attr,
@@ -187,9 +188,9 @@ int backlight_device_set_brightness(struct backlight_device *bd,
 		if (brightness > bd->props.max_brightness)
 			rc = -EINVAL;
 		else {
-			pr_debug("set brightness to %lu\n", brightness);
-			bd->props.brightness = brightness;
-			rc = backlight_update_status(bd);
+		pr_debug("set brightness to %lu\n", brightness);
+		bd->props.brightness = brightness;
+		rc = backlight_update_status(bd);
 		}
 	}
 	mutex_unlock(&bd->ops_lock);
