327822ae1c0d10464ec2b4d82237b866fe38163a
nullptr03 <nullptr03@singkolab.my.id>
2026-07-15 15:05:54 +0800

    techpack: camera: Import Xiaomi changes
    From taoyao-s-oss

--- techpack/camera/drivers/cam_sensor_module/cam_sensor_utils/cam_sensor_util.c
+++ techpack/camera/drivers/cam_sensor_module/cam_sensor_utils/cam_sensor_util.c
@@ -2428,11 +2428,6 @@ int cam_sensor_util_power_down(struct cam_sensor_power_ctrl_t *ctrl,
 		case SENSOR_STANDBY:
 		case SENSOR_CUSTOM_GPIO1:
 		case SENSOR_CUSTOM_GPIO2:
-
-			if (!gpio_num_info) {
-				CAM_ERR(CAM_SENSOR, "failed: No gpio");
-				continue;
-			}
 			if (!gpio_num_info->valid[pd->seq_type])
 				continue;
 
